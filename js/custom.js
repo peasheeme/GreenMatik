11d9a99740425344bef182066c2b438a3254ef3b
Perla Holguin <peasheeme@gmail.com>
2018-11-28 17:18:03 -0600

    inicio, refacciones, producto

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$("#owl-products").owlCarousel({
+    items : 5,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    autoPlay: 5000,
+    singleItem: false
+  });
+
+  $("#owl-refacciones").owlCarousel({
+    items : 1,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    autoPlay: 5000,
+    singleItem: true
+  });
